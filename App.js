ebf8c33110b85cd01c356ea3bf275a3acbf20fdd
Vikram madankar <madankarvikram@gmail.com>
2024-02-20 14:36:38 +0530

    Episode 01

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent = React.createElement("div", {id: "parent"},
+ [ React.createElement("div", {id: "child"},
+  [React.createElement("h1",{id:"h1tag"}, "I'm h1 tag"), 
+  React.createElement("h2", {id:"h2tag"}, "I'm h2 tag")]),
+  React.createElement("div", {id: "child2"},
+  [React.createElement("h1",{id:"h1tag"}, "I'm h1 tag"), 
+  React.createElement("h2", {id:"h2tag"}, "I'm h2 tag")])])
+
+  console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
